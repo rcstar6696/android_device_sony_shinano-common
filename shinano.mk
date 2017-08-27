f1aaecf8be0533949dba4b23710eab910df22b6c
Max Weffers <rcstar6696@gmail.com>
2017-08-27 12:40:50 +0200

    Revert "shinano: Add hw-binder-aware gatekeeper HIDL package"
    This reverts commit cb518b3f69c927dfb9416d4af305b81bb4904c3b.

--- shinano.mk
+++ shinano.mk
@@ -78,10 +78,6 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
      android.hardware.drm@1.0-impl
-     
-# Gatekeeper
-PRODUCT_PACKAGES += \
-	android.hardware.gatekeeper@1.0-impl
 
 # RIL
 PRODUCT_COPY_FILES += \
